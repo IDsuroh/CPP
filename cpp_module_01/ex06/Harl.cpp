0c8ef41892fffb84d6561b5ca87ab1f12aeaf004
suroh <suroh@student.42lisboa.com>
2025-05-07 14:23:04 +0100

    commit

--- cpp_module_01/ex06/Harl.cpp
+++ cpp_module_01/ex06/Harl.cpp
@@ -3,24 +3,32 @@
 
 void    Harl::debug( void )   {
     std::cout
-        << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!"
+        << "[ DEBUG ]" << std::endl
+        << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger." << std::endl
+        << "I really do!" << std::endl
         << std::endl;
 }
 
 void    Harl::info( void )    {
     std::cout
-        << "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!"
+        << "[ INFO ]" << std::endl
+        << "I cannot believe adding extra bacon costs more money." << std::endl 
+        << "You didn't put enough bacon in my burger!" << std::endl
+        << "If you did, I wouldn't be asking for more!" << std::endl
         << std::endl;
 }
 
 void    Harl::warning( void ) {
     std::cout
-        << "I think I deserve to have some extra bacon for free. I've been coming for years, whereas you started working here just last month."
+        << "[ WARNING ]" << std::endl
+        << "I think I deserve to have some extra bacon for free." << std::endl
+        << "I've been coming for years, whereas you started working here just last month." << std::endl
         << std::endl;
 }
 
 void    Harl::error( void )   {
     std::cout
+        << "[ ERROR ]" << std::endl
         << "This is unacceptable! I want to speak to the manager now."
         << std::endl;
 }
@@ -47,9 +55,12 @@ void    Harl::complain( std::string level ) {
     };
     for (size_t i = 0; i < sizeof(angerlevel)/sizeof(*angerlevel); ++i) {
         if (angerlevel[i] == level) {
-            (this->*funcs[i])();
+            while (i < sizeof(angerlevel)/sizeof(*angerlevel))  {
+                (this->*funcs[i])();
+                i++;
+            }
             return ;
         }
     }
-    std::cout << "[ Complaining to the Manager ]" << std::endl;
+    std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
 }
